6cdd2ea7cc1d955f040365c22bcca4330b5dd7ca
kavi-ta <kavisharma2807@gmail.com>
2022-08-04 22:41:03 +0530

    added blacklist function

--- erc20new.sol
+++ erc20new.sol
@@ -10,33 +10,53 @@ interface IERC20{
     function approve(address spender, uint amount) external returns(bool);
     function transferFrom( address sender, address recipient , uint amount ) external returns(bool);
     event Transfer(address indexed from , address indexed to , uint amount);
-    event Approval(address indexed owner , address indexed spender , uint amount);
+    event Approval(address indexed ownder , address indexed spender , uint amount);
 }
 
 contract ERC20 is IERC20{
     uint public totalSupply;
     mapping(address => uint) public balanceOf;
     mapping(address=> mapping(address=> uint)) public allowance;
-    address public blackList;
+    address[] public blackList;
 
     string public name = "Test";
     string public symbol = "TEST";
     uint8 public decimals = 18;
 
+    function blacklist(address user) public {
+        blackList.push(user);
+    }
+
+    function checkBlackList(address user) internal view returns(bool){
+        uint i;
+        for( i=0; i<blackList.length; i++){
+            if(blackList[i]==user){
+                return true;
+            }
+        }
+        return false;
+    }
     function transfer(address recipient , uint amount) external returns(bool){
+        require(checkBlackList(recipient) == false);
         balanceOf[msg.sender] -= amount;
         balanceOf[recipient]+=amount;
         emit Transfer(msg.sender, recipient, amount);
         return true;
+        
+        
+        
     }
     
     function approve(address spender, uint amount) external returns(bool){
-        allowance[msg.sender][spender] = amount;
-        emit Approval(msg.sender, spender, amount);
-        return true;
+        require(checkBlackList(spender)==false);
+        
+            allowance[msg.sender][spender] = amount;
+            emit Approval(msg.sender, spender, amount);
+            return true;
+        
+        
     }
     function transferFrom( address sender, address recipient , uint amount ) external returns(bool){
- 
         allowance[sender][msg.sender] -= amount;
         balanceOf[sender] -= amount;
         balanceOf[recipient]+= amount;
@@ -57,5 +77,10 @@ contract ERC20 is IERC20{
         emit Transfer( msg.sender,address(0) , amount);
     }
 
+    
+
+    
 
 }
+
+// https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol
